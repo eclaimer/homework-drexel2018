c1c64298f917f425e327548a300a41d22721a70b
eclaimer <1279861327@qq.com>
2020-07-18 17:06:44 +0800

    Create git1.py

---
+++ 320180940511-zhangbo/git1.py
@@ -0,0 +1,13 @@
+from subprocess import Popen, PIPE, check_output
+
+def gitFileDynamics(repo):
+    cmd = ['git', 'log', '--stat', '--oneline', '--follow', 'v4.4..v4.5', 'kernel/sched/core.c', '>', 'result.csv']
+    p = Popen(cmd, cwd=repo, stdout=PIPE, shell=True)
+    data, res = p.communicate()
+    # print the git output as one blob
+    #print(data.decode("utf-8"))
+    # ...or print it split into lines
+    for line in data.decode("utf-8").split("\n"):
+       print(line)
+
+gitFileDynamics(r'linux-stable')
